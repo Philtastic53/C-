d2343220e01de8f832c1561c8a5affac28892f83
Philtastic53 <122129193+Philtastic53@users.noreply.github.com>
2023-02-02 14:29:20 -0600

    Add files via upload
    These are additional C++ files to test uploading capabilities.

---
+++ extra.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is an extra file with extra lines." << endl
+    cout << "The purpose of this file is to help \n experiment with git." << endl;
+}
---
+++ goodbye.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Over and out!!" << endl;
+
+    cin.ignore();
+    cout << "Press any key to exit.";
+    cin.get();
+    return 0;
+}
